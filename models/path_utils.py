f1ec60f551c5c9a0e4d5d6a0dee9433c278051bf
chengq220 <chengqiu220@gmail.com>
2026-02-07 12:36:32 -0500

    fix

--- models/path_utils.py
+++ models/path_utils.py
@@ -17,7 +17,7 @@ SAM2_PATHS_CONFIG = {
 # ROBERTA_WEIGHTS_URL = 'https://dl.fbaipublicfiles.com/fairseq/models/roberta.base.tar.gz' # General text encoder 
 # ROBERTA_WEIGHTS_PATH = 'pretrain/roberta.base'
 
-ROBERTA_WEIGHTS_URL = 'https://dl.fbaipublicfiles.com/biolm/RoBERTa-base-PM-M3-Voc-distill-fairseq.tar.gz' # Medical text encoder
+ROBERTA_WEIGHTS_URL = 'https://dl.fbaipublicfiles.com/biolm/RoBERTa-base-PM-fairseq.tar.gz' # Medical text encoder
 ROBERTA_WEIGHTS_PATH = 'pretrain/RoBERTa-base-PM'
 ROBERTA_EXTRACT_PATH = 'pretrain/RoBERTa-base-PM-M3-Voc-distill/RoBERTa-base-PM-M3-Voc-distill-fairseq/'
 
