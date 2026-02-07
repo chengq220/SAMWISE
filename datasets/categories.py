04325df9438043e3f7a700b80c072f9bf8991b47
chengq220 <chengqiu220@gmail.com>
2026-02-06 21:22:52 -0500

    update tokenizer

--- datasets/categories.py
+++ datasets/categories.py
@@ -20,13 +20,13 @@ endovis2017_category_rev_dict = {
 }
 
 endovis2017_category_descriptor_dict = {
-	1: "Electrical surgical instrument with two-pronged tips",
-	2: "Articulated surgical gripper with multi-jaw design",
-	3: "Long suture holder with locking scissor handles",
-	4: "Vascular sealing device with parallel clamp jaws",
-	5: "Tissue holding tool with curved, blunt ends",
-	6: "Curved cutting device with insulated handles",
-	7: "Various surgical instruments with diverse forms"
+	1: "Bipolar Forceps Electrical surgical instrument with two-pronged tips",
+	2: "Prograsp Forceps Articulated surgical gripper with multi-jaw design",
+	3: "Large Needle Driver Long suture holder with locking scissor handles",
+	4: "Vessel Sealer Vascular sealing device with parallel clamp jaws",
+	5: "Grasping Retractor Tissue holding tool with curved, blunt ends",
+	6: "Monopolar Curved Scissors Curved cutting device with insulated handles",
+	7: "Other Various surgical instruments with diverse forms"
 }
 
 endovis2017_category_list = [
