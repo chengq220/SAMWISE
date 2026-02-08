193e399ef1121d9e5269b07ab81ee2c4556b560d
chengq220 <chengqiu220@gmail.com>
2026-02-08 09:31:11 -0500

    update

--- datasets/categories.py
+++ datasets/categories.py
@@ -19,14 +19,24 @@ endovis2017_category_rev_dict = {
     7: "Other"
 }
 
+# endovis2017_category_descriptor_dict = {
+# 	1: "Bipolar Forceps Electrical surgical instrument with two-pronged tips",
+# 	2: "Prograsp Forceps Articulated surgical gripper with multi-jaw design",
+# 	3: "Large Needle Driver Long suture holder with locking scissor handles",
+# 	4: "Vessel Sealer Vascular sealing device with parallel clamp jaws",
+# 	5: "Grasping Retractor Tissue holding tool with curved, blunt ends",
+# 	6: "Monopolar Curved Scissors Curved cutting device with insulated handles",
+# 	7: "Other Various surgical instruments with diverse forms"
+# }
+
 endovis2017_category_descriptor_dict = {
-	1: "Bipolar Forceps Electrical surgical instrument with two-pronged tips",
-	2: "Prograsp Forceps Articulated surgical gripper with multi-jaw design",
-	3: "Large Needle Driver Long suture holder with locking scissor handles",
-	4: "Vessel Sealer Vascular sealing device with parallel clamp jaws",
-	5: "Grasping Retractor Tissue holding tool with curved, blunt ends",
-	6: "Monopolar Curved Scissors Curved cutting device with insulated handles",
-	7: "Other Various surgical instruments with diverse forms"
+	1: "Bipolar Forceps ",
+	2: "Prograsp Forceps",
+	3: "Large Needle Driver",
+	4: "Vessel Sealer Vascular",
+	5: "Grasping Retractor",
+	6: "Monopolar Curved Scissors",
+	7: "Other"
 }
 
 endovis2017_category_list = [
---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#Install requirements
+pip install -r requirements.txt
+
+#Download data
+mkdir data
+cd data
+gdown --fuzzy "https://drive.google.com/file/d/1XG_lqhQZHBBHb1WM9s9TTu9Gbd4JbND7/view?usp=drive_link"
+tar -xvf endovis2017.tar
+
+#Download pretrain-models
+cd ..
+mkdir pretrain
+cd pretrain
+wget "https://dl.fbaipublicfiles.com/biolm/RoBERTa-base-PM-fairseq.tar.gz"
+tar -xzvf RoBERTa-base-PM-fairseq.tar.gz --no-same-owner
