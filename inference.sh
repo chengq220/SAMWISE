4315705c99703a59302e0e09aeac83963276420d
chengq220 <chengqiu220@gmail.com>
2026-02-11 17:40:38 -0500

    change inference

--- inference.sh
+++ inference.sh
@@ -4,6 +4,7 @@ python inference.py \
     --sam2_version 'med' \
     --input_path data/endovis2017/val1/image \
     --resume output/endovis2017_med/checkpoint0004.pth \
+    --num_frames 8 \
     --text_prompts "Bipolar Forceps" \
     --threshold 0.8 \
     --HSA \
